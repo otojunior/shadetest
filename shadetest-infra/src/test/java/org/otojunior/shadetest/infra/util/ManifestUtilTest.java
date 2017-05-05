ee97c6a804de77d8c321048762b025ab90aca5c9
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2017-05-05 14:29:11 -0300

    Correção de cobertura de testes do Sonar

---
+++ shadetest-infra/src/test/java/org/otojunior/shadetest/infra/util/ManifestUtilTest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.otojunior.shadetest.infra.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author 01456231650
+ *
+ */
+public class ManifestUtilTest {
+
+	/**
+	 * Test method for {@link org.otojunior.shadetest.infra.util.ManifestUtil#getAttribute(java.lang.String)}.
+	 */
+	@Test
+	public final void testGetAttributeString() {
+		String attribute = ManifestUtil.getAttribute(ManifestUtil.BUILD_JDK);
+		assertEquals("1.8.0_111", attribute);
+	}
+
+	/**
+	 * Test method for {@link org.otojunior.shadetest.infra.util.ManifestUtil#getAttribute(java.lang.String, boolean)}.
+	 */
+	@Test
+	public final void testGetAttributeStringBoolean() {
+		String attribute = ManifestUtil.getAttribute(ManifestUtil.BUILD_JDK, true);
+		assertEquals("1.8.0_111", attribute);
+	}
+}
