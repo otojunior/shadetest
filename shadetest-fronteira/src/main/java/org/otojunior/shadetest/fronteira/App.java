e429cefa8ab489ee74e4a3d8646115cdf70c2e15
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2017-07-20 16:03:40 -0300

    Implementação das imagens nas telas e Launch4J

--- shadetest-fronteira/src/main/java/org/otojunior/shadetest/fronteira/App.java
+++ shadetest-fronteira/src/main/java/org/otojunior/shadetest/fronteira/App.java
@@ -1,5 +1,10 @@
 package org.otojunior.shadetest.fronteira;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileLock;
+
 import org.otojunior.shadetest.fronteira.ui.JanelaPrincipal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,9 +22,38 @@ public class App {
 	/**
 	 * Application main method.
 	 * @param args Command line arguments.
+	 * @throws IOException 
 	 */
 	public static void main(String[] args) {
 		LOG.info("shadetest Application.");
+		
+		/*
+		 * Proteção contra múltiplas instâncias em execução. 
+		 */
+		if (isProgramaExecutando()) {
+			LOG.info("Uma instância da aplicação ShadeTest, já se encontra em execução");
+			System.exit(1);
+		}
+		
 		new JanelaPrincipal().setVisible(true);
 	}
+	
+	/**
+     * Verifica se a aplicação está rodando. 
+     * Este método é útil quando há várias versões instaladas
+     * e somente uma deve executar por vez. 
+     * @return true se o programa estiver executando.
+     */
+    private static boolean isProgramaExecutando() {
+        FileLock lock = null;
+        File file = new File(System.getProperty("user.home"), ".shadetestlock");
+        RandomAccessFile randomAccessFile = null;
+        try {
+        	randomAccessFile = new RandomAccessFile(file, "rw");
+            lock = randomAccessFile.getChannel().tryLock();
+        } catch (IOException e) {
+        	LOG.error(e.getMessage());
+        }
+        return (lock == null);
+    }
 }
