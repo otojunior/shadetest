3cc4441038be40e78c9638aefef0dad8fdaadfcd
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2017-07-24 10:02:01 -0300

    Preparação para teste de Java embutido

--- shadetest-fronteira/src/main/java/org/otojunior/shadetest/fronteira/ui/JanelaPrincipal.java
+++ shadetest-fronteira/src/main/java/org/otojunior/shadetest/fronteira/ui/JanelaPrincipal.java
@@ -4,12 +4,14 @@
 package org.otojunior.shadetest.fronteira.ui;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 import org.otojunior.shadetest.dominio.service.TextFileReaderService;
 import org.slf4j.Logger;
@@ -29,33 +31,49 @@ public class JanelaPrincipal extends JFrame {
 	 * Construtor padrão.
 	 */
 	public JanelaPrincipal() {
-		this.setSize(200, 100);
+		this.setSize(300, 150);
 		this.setLocation(500,  500);
 		this.setResizable(false);
 		this.setTitle("Janela Principal - Teste Shade");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JButton btnOk = new JButton("OK");
-		btnOk.addActionListener(e -> {
-			TextFileReaderService service = new TextFileReaderService();
-			String result = service.read();
-			JOptionPane.showMessageDialog(
-				JanelaPrincipal.this, 
-				result);
+		btnOk.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				TextFileReaderService service = new TextFileReaderService();
+				String result = service.read();
+				JOptionPane.showMessageDialog(
+					JanelaPrincipal.this, 
+					result);
+			}
 		});
 		
 		JButton btnCancelar = new JButton("Cancelar");
-		btnCancelar.addActionListener(e -> {
-			JanelaPrincipal.this.setVisible(false);
-			JanelaPrincipal.this.dispose();
-			System.exit(0);
+		btnCancelar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JanelaPrincipal.this.setVisible(false);
+				JanelaPrincipal.this.dispose();
+				System.exit(0);
+			}
 		});
 		
 		JPanel painelBotoes = new JPanel();
 		painelBotoes.add(btnOk);
 		painelBotoes.add(btnCancelar);
 		
+		String str = 
+			"java.version: " + System.getProperty("java.version") + "\n" +
+			"java.home: " + System.getProperty("java.home") + "\n" +
+			"java.vm.name: " + System.getProperty("java.vm.name") + "\n" +
+			"java.class.version: " + System.getProperty("java.class.version");
+		
+		JTextArea textArea = new JTextArea(str);
+		textArea.setEditable(false);
+		textArea.setAutoscrolls(false);
+		
 		this.add(painelBotoes, BorderLayout.SOUTH);
-		this.add(new JLabel("Teste Shade"), BorderLayout.CENTER);
+		this.add(textArea, BorderLayout.CENTER);
 	}
 }
